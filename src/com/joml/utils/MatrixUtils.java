53acad73153c6650333c82b360872cdeb1fde2db
Richard Greenlees <r_ja_greenlees@hotmail.com>
2015-02-27 09:52:03 +0000

    Added MatrixUtils for easy calculation of commonly-used matrices e.g. model matrix

--- src/com/joml/utils/MatrixUtils.java
+++ src/com/joml/utils/MatrixUtils.java
@@ -1,7 +1,20 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * (C) Copyright 2015 Richard Greenlees
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ * and associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ *  1) The above copyright notice and this permission notice shall be included
+ *     in all copies or substantial portions of the Software.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
  */
 package com.joml.utils;
 
@@ -11,8 +24,11 @@ import com.joml.vector.Vector3f;
 import java.nio.FloatBuffer;
 
 /**
- *
- * @author RGreenlees
+ * MatrixUtils
+ * 
+ * Contains useful functions for generating matrices that aren't to do with the camera (use CamMath for that)
+ * 
+ * @author Richard Greenlees
  */
 public class MatrixUtils {
     
