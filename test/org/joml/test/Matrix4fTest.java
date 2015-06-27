76df48ec07fed5277290f4362f1c3d0bcfa11d22
httpdigest <kburjack@googlemail.com>
2015-06-27 12:18:11 +0200

    Add test for lookAt

--- test/org/joml/test/Matrix4fTest.java
+++ test/org/joml/test/Matrix4fTest.java
@@ -5,7 +5,7 @@ import java.nio.IntBuffer;
 import junit.framework.TestCase;
 
 import org.joml.Matrix4f;
-import org.joml.Vector4f;
+import org.joml.Vector3f;
 
 /**
  * Tests for the {@link Matrix4f} class.
@@ -21,21 +21,26 @@ public class Matrix4fTest extends TestCase {
         /* Build some arbitrary viewport. */
         IntBuffer viewport = IntBuffer.wrap(new int[]{0, 0, 800, 800});
 
-        Vector4f expected = new Vector4f(1.0f, 2.0f, -3.0f, 1.0f);
-        Vector4f actual = new Vector4f();
+        Vector3f expected = new Vector3f(1.0f, 2.0f, -3.0f);
+        Vector3f actual = new Vector3f();
         Matrix4f inverse = new Matrix4f();
 
         /* Build a perspective projection and then project and unproject. */
         new Matrix4f()
         .perspective(45.0f, 1.0f, 0.01f, 100.0f)
-        .project(expected.x, expected.y, expected.z, viewport, actual)
-        .unproject(actual.x, actual.y, actual.z, viewport, inverse, actual);
+        .project(expected, viewport, actual)
+        .unproject(actual, viewport, inverse, actual);
 
         /* Check for equality of the components */
         assertEquals(expected.x, actual.x, TestUtil.MANY_OPS_AROUND_ZERO_PRECISION_FLOAT);
         assertEquals(expected.y, actual.y, TestUtil.MANY_OPS_AROUND_ZERO_PRECISION_FLOAT);
         assertEquals(expected.z, actual.z, TestUtil.MANY_OPS_AROUND_ZERO_PRECISION_FLOAT);
-        assertEquals(expected.w, actual.w, TestUtil.MANY_OPS_AROUND_ZERO_PRECISION_FLOAT);
+    }
+
+    public void testLookAt() {
+        Matrix4f m1 = new Matrix4f().lookAt(0.0f, 2.0f, 3.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f);
+        Matrix4f m2 = new Matrix4f().translate(0.0f, 0.0f, -(float) Math.sqrt(2*2 + 3*3)).rotateX((float) Math.toDegrees(Math.atan2(2, 3)));
+        TestUtil.assertMatrix4fEquals(m1, m2, TestUtil.MANY_OPS_AROUND_ZERO_PRECISION_FLOAT);
     }
 
 }
