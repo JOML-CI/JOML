1ed3efce0f6f3b0c9695308598ae36bf05d5f882
httpdigest <kburjack@googlemail.com>
2015-07-10 14:35:09 +0200

    That test main() method snuck in again

--- src/org/joml/Matrix4f.java
+++ src/org/joml/Matrix4f.java
@@ -5595,20 +5595,6 @@ public class Matrix4f implements Externalizable {
         return plane;
     }
 
-    public static void main(String[] args) {
-        Matrix4f m = new Matrix4f();
-        {
-            long time1 = System.nanoTime();
-            int res = -1;
-            for (int i = 0; i < 9000000; i++)
-                res = m.isAabInsideFrustumMasked(0.5f, 0.5f, -5.5f,
-                                                 0.7f, 0.6f, -4.6f, 1<<4);
-            long time2 = System.nanoTime();
-            System.err.println("Took " + (time2 - time1) / 1E6);
-            System.err.println(res);
-        }
-    }
-
     /**
      * Obtain the direction of a ray starting at the center of the coordinate system and going 
      * through the near frustum plane.
