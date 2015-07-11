b63d14ffe2439614e035695e8525580c94fc4f7e
httpdigest <kburjack@googlemail.com>
2015-07-11 21:47:19 +0200

    Fix Quaternion.slerp() and add test
    Also formatting

--- test/org/joml/test/Vector3fTest.java
+++ test/org/joml/test/Vector3fTest.java
@@ -8,18 +8,18 @@ import org.joml.*;
  * @author Sebastian Fellner
  */
 public class Vector3fTest extends TestCase {
-	
-	public void testAngleVector3fVector3f() {
-		Vector3f testVec1 = new Vector3f(2f, -9.37f, 5.892f);
-		Vector3f testVec2 = new Vector3f();
-		
-		// angle(v, v) should give 0
-		float angle = testVec1.angle(testVec1);
-		assertEquals(0, angle, TestUtil.MANY_OPS_AROUND_ZERO_PRECISION_FLOAT);
-		
-		// angle(v, -v) should give Math.PI
-		testVec1.negate(testVec2);
-		angle = testVec1.angle(testVec2);
-		assertEquals(Math.PI, angle, TestUtil.MANY_OPS_AROUND_ZERO_PRECISION_FLOAT);
-	}
+    
+    public void testAngleVector3fVector3f() {
+        Vector3f testVec1 = new Vector3f(2f, -9.37f, 5.892f);
+        Vector3f testVec2 = new Vector3f();
+        
+        // angle(v, v) should give 0
+        float angle = testVec1.angle(testVec1);
+        assertEquals(0, angle, TestUtil.MANY_OPS_AROUND_ZERO_PRECISION_FLOAT);
+        
+        // angle(v, -v) should give Math.PI
+        testVec1.negate(testVec2);
+        angle = testVec1.angle(testVec2);
+        assertEquals(Math.PI, angle, TestUtil.MANY_OPS_AROUND_ZERO_PRECISION_FLOAT);
+    }
 }
