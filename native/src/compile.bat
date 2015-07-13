5f7e35e80ffda972729b3ce4a80ad6872d446b0d
httpdigest <kburjack@googlemail.com>
2015-07-13 23:03:26 +0200

    Call FlushInstructionCache() after generating executable code page
    See
    https://msdn.microsoft.com/en-us/library/windows/desktop/aa366887%28v=vs.85%29.aspx

--- native/src/compile.bat
+++ native/src/compile.bat
@@ -1,4 +1,4 @@
 minilua.exe dynasm\dynasm.lua -o codegen.win64.c -D X64 codegen.dasc
-gcc -shared -o joml.dll -I"C:\Program Files\Java\jdk1.8.0_45\include" -I"C:\Program Files\Java\jdk1.8.0_45\include\win32" codegen.win64.c NativeVector4f.c
+gcc -O3 -shared -o joml.dll -I"C:\Program Files\Java\jdk1.8.0_45\include" -I"C:\Program Files\Java\jdk1.8.0_45\include\win32" codegen.win64.c NativeVector4f.c
 strip -x -s joml.dll
 upx joml.dll
