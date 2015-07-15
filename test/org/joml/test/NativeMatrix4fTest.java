af10241291027d3ea66fc348a7fac773584b53b5
httpdigest <kburjack@googlemail.com>
2015-07-15 22:01:56 +0200

    Add more tests

--- test/org/joml/test/NativeMatrix4fTest.java
+++ test/org/joml/test/NativeMatrix4fTest.java
@@ -22,4 +22,39 @@ public class NativeMatrix4fTest extends TestCase {
         TestUtil.assertMatrix4fEquals(expected, actual, 0.0f);
     }
 
+    public void testRotateZ() {
+        Sequence seq = new Sequence();
+        NativeMatrix4f nm = new NativeMatrix4f(seq);
+        {
+            nm.identity();
+            nm.rotateZ((float) Math.PI);
+        }
+        seq.terminate();
+        seq.call();
+        Matrix4f actual = new Matrix4f();
+        nm.get(actual);
+        Matrix4f expected = new Matrix4f().rotateZ((float) Math.PI);
+        TestUtil.assertMatrix4fEquals(expected, actual, 0.0f);
+    }
+
+    public void testMulMatrix() {
+        Sequence seq = new Sequence();
+        NativeMatrix4f nm = new NativeMatrix4f(seq);
+        NativeMatrix4f nm2 = new NativeMatrix4f(seq);
+        {
+            nm.identity().rotateZ(0.123f);
+            nm2.identity().rotateZ(0.23f);
+            nm.mul(nm2);
+        }
+        seq.terminate();
+        seq.call();
+        Matrix4f actual = new Matrix4f();
+        nm.get(actual);
+        Matrix4f expected = new Matrix4f();
+        new Matrix4f()
+          .rotateZ(0.123f)
+          .mul(new Matrix4f().rotateZ(0.23f), expected);
+        TestUtil.assertMatrix4fEquals(expected, actual, 0.0f);
+    }
+
 }
