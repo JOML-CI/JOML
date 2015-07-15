50f03afe98ae6f06b56785311a9a330fe14610ce
httpdigest <kburjack@googlemail.com>
2015-07-15 23:21:22 +0200

    Test what happens when calling terminate() on a terminated Sequence

--- test/org/joml/test/NativeMatrix4fTest.java
+++ test/org/joml/test/NativeMatrix4fTest.java
@@ -100,4 +100,31 @@ public class NativeMatrix4fTest extends TestCase {
         }
     }
 
+    /**
+     * Calls a sequence of matrix operations on the same {@link Sequence} twice.
+     * <p>
+     * This should check that {@link Sequence#terminate()} is actually a no-op
+     * when the {@link Sequence} is already terminated.
+     * And this is so that client code can remain the same for both non-terminated
+     * and terminated sequences.
+     */
+    public void testMulMatrixTwoTimes() {
+        Sequence seq = new Sequence();
+        NativeMatrix4f nm = new NativeMatrix4f(seq);
+        NativeMatrix4f nm2 = new NativeMatrix4f(seq);
+        for (int i = 0; i < 2; i++) {
+            nm.identity().rotateZ(0.123f);
+            nm2.identity().rotateZ(0.23f);
+            nm.mul(nm2);
+            seq.terminate().call();
+        }
+        Matrix4f actual = new Matrix4f();
+        nm.get(actual);
+        Matrix4f expected = new Matrix4f();
+        new Matrix4f()
+          .rotateZ(0.123f)
+          .mul(new Matrix4f().rotateZ(0.23f), expected);
+        TestUtil.assertMatrix4fEquals(expected, actual, 0.0f);
+    }
+
 }
