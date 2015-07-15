f04a5d437732837705ad63c2732606bbddcf443b
httpdigest <kburjack@googlemail.com>
2015-07-15 21:56:02 +0200

    Add license header and some JavaDocs

--- src/org/joml/NativeMatrix4f.java
+++ src/org/joml/NativeMatrix4f.java
@@ -1,3 +1,25 @@
+/*
+ * (C) Copyright 2015 Richard Greenlees
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+
+ */
 package org.joml;
 
 import java.nio.Buffer;
@@ -11,12 +33,14 @@ import java.nio.FloatBuffer;
  * <p>
  * The API is almost identical to the {@link Matrix4f}, only that the
  * {@link NativeMatrix4f} does not execute the operations immediately but
- * instead stores them for subsequent batch execution by a runtime-generated
- * native function.
+ * instead stores them in a {@link Sequence} for subsequent batch execution
+ * by a runtime-generated native function.
  * <p>
  * The {@link NativeMatrix4f} is therefore a convenient interface to the
- * underlying JIT engine in order to generate native functions using known
- * matrix operations.
+ * underlying JIT engine in order to generate native matrix functions that operate
+ * on <code>this</code>.
+ * 
+ * @since 2.0.0
  * 
  * @author Kai Burjack
  */
