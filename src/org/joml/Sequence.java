9213a709e2356d362ba938f158b47854109a189a
httpdigest <kburjack@googlemail.com>
2015-07-21 01:23:53 +0200

    Need 0.0 for scale.w

--- src/org/joml/Sequence.java
+++ src/org/joml/Sequence.java
@@ -367,7 +367,7 @@ public class Sequence {
         // always put 4 floats for nice alignment for movaps
         putArg(tx).putArg(ty).putArg(tz).putArg(0.0f);
         putArg(qx).putArg(qy).putArg(qz).putArg(qw);
-        putArg(sx).putArg(sy).putArg(sz).putArg(1.0f);
+        putArg(sx).putArg(sy).putArg(sz).putArg(0.0f);
         putArg(matrix.matrixBufferAddr);
         putArg(0L);
         return this;
