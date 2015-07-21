e8f6aaa8de6de4cb4f29145b03beee53a8c1ffa5
httpdigest <kburjack@googlemail.com>
2015-07-21 14:17:26 +0200

    Make linux version

--- native/src/compile_linear.bat
+++ native/src/compile_linear.bat
@@ -3,7 +3,7 @@
 :: - upx.exe in PATH
 :: - JAVA_HOME pointing to a JDK
 
-minilua.exe dynasm\dynasm.lua -o codegen_linear.win64.c -D X64 codegen_linear.dasc
+minilua.exe dynasm\dynasm.lua -o codegen_linear.win64.c -D X64 codegen_linear_win64.dasc
 gcc -O3 -shared -o joml.dll -I"%JAVA_HOME%\include" -I"%JAVA_HOME%\include\win32" codegen_linear.win64.c Native.c
 strip -x -s joml.dll
 upx joml.dll
