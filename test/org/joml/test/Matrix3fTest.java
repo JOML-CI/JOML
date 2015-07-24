cd193a4811936031f66b4269112e4b45841f5c57
httpdigest <kburjack@googlemail.com>
2015-07-24 13:10:50 +0200

    Add some tests for Matrix3f

---
+++ test/org/joml/test/Matrix3fTest.java
@@ -0,0 +1,38 @@
+package org.joml.test;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+import org.joml.Matrix3f;
+import org.joml.Vector2f;
+
+public class Matrix3fTest extends TestCase {
+
+    public void testRotate() {
+        Matrix3f m = new Matrix3f().rotate((float) Math.toRadians(90));
+        Vector2f expected = new Vector2f(-1.0f, 1.0f);
+        Vector2f actual = new Vector2f(1.0f, 1.0f);
+        m.transform(actual);
+        Assert.assertEquals(expected.x, actual.x, 1E-7f);
+        Assert.assertEquals(expected.y, actual.y, 1E-7f);
+    }
+
+    public void testTranslate() {
+        Matrix3f m = new Matrix3f().translate(3.0f, -7.0f);
+        Vector2f expected = new Vector2f(4.0f, -6.0f);
+        Vector2f actual = new Vector2f(1.0f, 1.0f);
+        m.transform(actual);
+        Assert.assertEquals(expected.x, actual.x, 1E-7f);
+        Assert.assertEquals(expected.y, actual.y, 1E-7f);
+    }
+
+    public void testScale() {
+        Matrix3f m = new Matrix3f().scale(3.0f, -2.0f);
+        Vector2f expected = new Vector2f(1.5f, -4.0f);
+        Vector2f actual = new Vector2f(0.5f, 2.0f);
+        m.transform(actual);
+        Assert.assertEquals(expected.x, actual.x, 1E-7f);
+        Assert.assertEquals(expected.y, actual.y, 1E-7f);
+    }
+
+}
