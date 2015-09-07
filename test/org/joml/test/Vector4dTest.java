5bf8cd141d8eea52651d2f2cc6f536aea3397f17
Immortius <immortius@gmail.com>
2015-09-07 19:17:57 +1000

    Fixes an issue with Vector4d's mul(double, Vector4d) and div(double, Vector4d) - they incorrectly alter the called object rather than update the provided destination Vector4d.

--- test/org/joml/test/Vector4dTest.java
+++ test/org/joml/test/Vector4dTest.java
@@ -22,4 +22,22 @@ public class Vector4dTest extends TestCase {
         angle = testVec1.angle(testVec2);
         assertEquals(Math.PI, angle, TestUtil.MANY_OPS_AROUND_ZERO_PRECISION_DOUBLE);
     }
+
+    public void testMulByScalarIntoDest() {
+        Vector4d testVec1 = new Vector4d(2, 2, 2, 2);
+        Vector4d destVec = new Vector4d(0, 0, 0, 0);
+
+        testVec1.mul(2, destVec);
+        assertEquals(new Vector4d(4, 4, 4, 4), destVec);
+        assertEquals(new Vector4d(2, 2, 2, 2), testVec1);
+    }
+
+    public void testDivByScalarIntoDest() {
+        Vector4d testVec1 = new Vector4d(2, 2, 2, 2);
+        Vector4d destVec = new Vector4d(0, 0, 0, 0);
+
+        testVec1.div(2, destVec);
+        assertEquals(new Vector4d(1, 1, 1, 1), destVec);
+        assertEquals(new Vector4d(2, 2, 2, 2), testVec1);
+    }
 }
