ec8f17c080b0d289b7ca7f17b27a260ae6dd5f63
httpdigest <kburjack@googlemail.com>
2015-10-17 13:34:46 +0200

    Fix Matrix4f.determinant3x3() and use it again for normal()

--- src/org/joml/Matrix4f.java
+++ src/org/joml/Matrix4f.java
@@ -1118,9 +1118,9 @@ public class Matrix4f implements Externalizable {
      * @return the determinant
      */
     public float determinant3x3() {
-        return m00 * (m11 * m22 - m12 * m21)
-             + m01 * (m12 * m20 - m10 * m22)
-             + m02 * (m01 * m21 - m11 * m20);
+        return (m00 * m11 - m01 * m10) * m22
+             + (m02 * m10 - m00 * m12) * m21
+             + (m01 * m12 - m02 * m11) * m20;
     }
 
     /**
@@ -6335,7 +6335,7 @@ public class Matrix4f implements Externalizable {
      * @return dest
      */
     public Matrix4f normal(Matrix4f dest) {
-        float det = determinant();
+        float det = determinant3x3();
         float s = 1.0f / det;
         /* Invert and transpose in one go */
         dest.set((m11 * m22 - m21 * m12) * s,
@@ -6372,7 +6372,7 @@ public class Matrix4f implements Externalizable {
      * @return dest
      */
     public Matrix3f normal(Matrix3f dest) {
-        float det = determinant();
+        float det = determinant3x3();
         float s = 1.0f / det;
         /* Invert and transpose in one go */
         dest.m00 = (m11 * m22 - m21 * m12) * s;
