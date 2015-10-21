44febcb18a54ae085e0e8a1c09e02d843b41475a
httpdigest <kburjack@googlemail.com>
2015-10-21 19:45:17 +0200

    Fix JavaDocs of Matrix4.normal(Matrix3)

--- src/org/joml/Matrix4d.java
+++ src/org/joml/Matrix4d.java
@@ -4747,10 +4747,11 @@ public class Matrix4d implements Externalizable {
      * <p>
      * Please note that, if <code>this</code> is an orthogonal matrix or a matrix whose columns are orthogonal vectors, 
      * then this method <i>need not</i> be invoked, since in that case <code>this</code> itself is its normal matrix.
-     * In that case, use {@link #set(Matrix3d)} to set a given Matrix3d to only the upper left 3x3 submatrix
+     * In that case, use {@link Matrix3d#set(Matrix4d)} to set a given Matrix3f to only the upper left 3x3 submatrix
      * of this matrix.
      * 
-     * @see #set(Matrix3d)
+     * @see Matrix3d#set(Matrix4d)
+     * @see #get3x3(Matrix3d)
      * 
      * @param dest
      *             will hold the result
