8dedf31239cb73b7ee01e32aaca3201005615a6d
httpdigest <kburjack@googlemail.com>
2016-01-19 21:15:28 +0100

    Give fastmath its own artifact name

--- src/org/joml/Math.java
+++ src/org/joml/Math.java
@@ -1,5 +1,5 @@
 /*
- * (C) Copyright 2015 JOML
+ * (C) Copyright 2015-2016 JOML
 
  Permission is hereby granted, free of charge, to any person obtaining a copy
  of this software and associated documentation files (the "Software"), to deal
