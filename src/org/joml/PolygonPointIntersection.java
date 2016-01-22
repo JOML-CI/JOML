c423aacfab16151598b90732259b11b4c7b10e62
httpdigest <kburjack@googlemail.com>
2016-01-22 21:25:58 +0100

    Add first simple polygon/point intersection solution with caching

---
+++ src/org/joml/PolygonPointIntersection.java
@@ -0,0 +1,90 @@
+package org.joml;
+
+/**
+ * Class for polygon/point intersection tests when testing many points against the same static polygon.
+ * <p>
+ * It can handle concave polygons.
+ * <p>
+ * Reference: <a href="http://alienryderflex.com/polygon/">http://alienryderflex.com</a>
+ * 
+ * @author Kai Burjack
+ */
+public class PolygonPointIntersection {
+
+    private float[] verticesXY;
+    private float[] constant;
+    private float[] multiple;
+
+    /**
+     * Create a new {@link PolygonPointIntersection} object with the given polygon vertices.
+     * <p>
+     * The <code>verticesXY</code> array contains the x and y coordinates of all vertices. This array will not be copied so its content must remain constant for
+     * as long as the PolygonPointIntersection is used with it.
+     * 
+     * @param verticesXY
+     *            contains the x and y coordinates of all vertices
+     */
+    public PolygonPointIntersection(float[] verticesXY) {
+        set(verticesXY);
+    }
+
+    /**
+     * Reinitialize this {@link PolygonPointIntersection} object with the given polygon vertices.
+     * <p>
+     * The <code>verticesXY</code> array contains the x and y coordinates of all vertices. This array will not be copied so its content must remain constant for
+     * as long as the PolygonPointIntersection is used with it.
+     * 
+     * @param verticesXY
+     *            contains the x and y coordinates of all vertices
+     */
+    public void set(float[] verticesXY) {
+        this.verticesXY = verticesXY;
+        if (this.constant == null || this.constant.length < verticesXY.length / 2) {
+            this.constant = new float[verticesXY.length / 2];
+            this.multiple = new float[verticesXY.length / 2];
+        }
+        precalcValues();
+    }
+
+    private void precalcValues() {
+        int i, j = verticesXY.length / 2 - 1;
+        for (i = 0; i < verticesXY.length / 2; i++) {
+            float yi = verticesXY[2 * i + 1];
+            float yj = verticesXY[2 * j + 1];
+            float xi = verticesXY[2 * i + 0];
+            if (yj == yi) {
+                constant[i] = xi;
+                multiple[i] = 0.0f;
+            } else {
+                float xj = verticesXY[2 * j + 0];
+                constant[i] = xi - (yi * xj) / (yj - yi) + (yi * xi) / (yj - yi);
+                multiple[i] = (xj - xi) / (yj - yi);
+            }
+            j = i;
+        }
+    }
+
+    /**
+     * Test whether the given point <tt>(x, y)</tt> lies inside the polygon stored in this {@link PolygonPointIntersection} object.
+     * 
+     * @param x
+     *            the x coordinate of the point to test
+     * @param y
+     *            the y coordinate of the point to test
+     * @return <code>true</code> iff the point lies inside the polygon; <code>false</code> otherwise
+     */
+    public boolean pointInPolygon(float x, float y) {
+        int i, j = verticesXY.length / 2 - 1;
+        boolean oddNodes = false;
+        for (i = 0; i < verticesXY.length / 2; i++) {
+            float yi = verticesXY[2 * i + 1];
+            float yj = verticesXY[2 * j + 1];
+            if ((yi <= y && yj >= y || yj <= y && yi >= y)) {
+                oddNodes ^= (y * multiple[i] + constant[i] <= x);
+            }
+            j = i;
+        }
+        return oddNodes;
+    }
+
+}
