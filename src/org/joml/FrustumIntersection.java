27e704babde843587834da5bcf4ca6db0c09d2bd
httpdigest <kburjack@googlemail.com>
2016-02-02 16:24:46 +0100

    Distinguish between simple inside/outside tests and intersection tests
    Also some minor JavaDocs fixes

--- src/org/joml/FrustumIntersection.java
+++ src/org/joml/FrustumIntersection.java
@@ -80,6 +80,11 @@ public class FrustumIntersection {
      * indicating that the axis-aligned box is fully inside of the frustum.  
      */
     public static final int INSIDE = -2;
+    /**
+     * Return value of {@link #intersectSphere(Vector3f, float)} or {@link #intersectSphere(float, float, float, float)}
+     * indicating that the sphere is completely outside of the frustum.
+     */
+    public static final int OUTSIDE = -3;
 
     /**
      * The value in a bitmask for
@@ -181,19 +186,18 @@ public class FrustumIntersection {
     }
 
     /**
-     * Determine whether the given point is within the viewing frustum
-     * defined by <code>this</code> frustum culler.
+     * Test whether the given point is within the frustum defined by <code>this</code> frustum culler.
      * 
      * @param point
      *          the point to test
-     * @return <code>true</code> if the given point is inside the clipping frustum; <code>false</code> otherwise
+     * @return <code>true</code> if the given point is inside the frustum; <code>false</code> otherwise
      */
     public boolean testPoint(Vector3f point) {
         return testPoint(point.x, point.y, point.z);
     }
 
     /**
-     * Determine whether the given point <tt>(x, y, z)</tt> is within the viewing frustum defined by <code>this</code> frustum culler.
+     * Test whether the given point <tt>(x, y, z)</tt> is within the frustum defined by <code>this</code> frustum culler.
      * 
      * @param x
      *          the x-coordinate of the point
@@ -201,7 +205,7 @@ public class FrustumIntersection {
      *          the y-coordinate of the point
      * @param z
      *          the z-coordinate of the point
-     * @return <code>true</code> if the given point is inside the clipping frustum; <code>false</code> otherwise
+     * @return <code>true</code> if the given point is inside the frustum; <code>false</code> otherwise
      */
     public boolean testPoint(float x, float y, float z) {
         return nxX * x + nxY * y + nxZ * z + nxW >= 0 &&
@@ -213,7 +217,7 @@ public class FrustumIntersection {
     }
 
     /**
-     * Determine whether the given sphere is partly or completely within the viewing frustum defined by <code>this</code> frustum culler.
+     * Test whether the given sphere is partly or completely within or outside of the frustum defined by <code>this</code> frustum culler.
      * <p>
      * The algorithm implemented by this method is conservative. This means that in certain circumstances a <i>false positive</i>
      * can occur, when the method returns <tt>true</tt> for spheres that are actually not visible.
@@ -223,7 +227,7 @@ public class FrustumIntersection {
      *          the sphere's center
      * @param radius
      *          the sphere's radius
-     * @return <code>true</code> if the given sphere is partly or completely inside the clipping frustum;
+     * @return <code>true</code> if the given sphere is partly or completely inside the frustum;
      *         <code>false</code> otherwise
      */
     public boolean testSphere(Vector3f center, float radius) {
@@ -231,7 +235,7 @@ public class FrustumIntersection {
     }
 
     /**
-     * Determine whether the given sphere is partly or completely within the viewing frustum defined by <code>this</code> frustum culler.
+     * Test whether the given sphere is partly or completely within or outside of the frustum defined by <code>this</code> frustum culler.
      * <p>
      * The algorithm implemented by this method is conservative. This means that in certain circumstances a <i>false positive</i>
      * can occur, when the method returns <tt>true</tt> for spheres that are actually not visible.
@@ -245,7 +249,7 @@ public class FrustumIntersection {
      *          the z-coordinate of the sphere's center
      * @param r
      *          the sphere's radius
-     * @return <code>true</code> if the given sphere is partly or completely inside the clipping frustum;
+     * @return <code>true</code> if the given sphere is partly or completely inside the frustum;
      *         <code>false</code> otherwise
      */
     public boolean testSphere(float x, float y, float z, float r) {
@@ -258,7 +262,137 @@ public class FrustumIntersection {
     }
 
     /**
-     * Determine whether the given axis-aligned box is partly or completely within the viewing frustum defined by <code>this</code> frustum culler
+     * Determine whether the given sphere is partly or completely within or outside of the frustum defined by <code>this</code> frustum culler.
+     * <p>
+     * The algorithm implemented by this method is conservative. This means that in certain circumstances a <i>false positive</i>
+     * can occur, when the method returns <tt>true</tt> for spheres that are actually not visible.
+     * See <a href="http://iquilezles.org/www/articles/frustumcorrect/frustumcorrect.htm">iquilezles.org</a> for an examination of this problem.
+     * 
+     * @param center
+     *          the sphere's center
+     * @param radius
+     *          the sphere's radius
+     * @return {@link #INSIDE} if the given sphere is completely inside the frustum, or {@link #INTERSECT} if the sphere intersects
+     *         the frustum, or {@link #OUTSIDE} if the sphere is outside of the frustum
+     */
+    public int intersectSphere(Vector3f center, float radius) {
+        return intersectSphere(center.x, center.y, center.z, radius);
+    }
+
+    /**
+     * Determine whether the given sphere is partly or completely within or outside of the frustum defined by <code>this</code> frustum culler.
+     * <p>
+     * The algorithm implemented by this method is conservative. This means that in certain circumstances a <i>false positive</i>
+     * can occur, when the method returns <tt>true</tt> for spheres that are actually not visible.
+     * See <a href="http://iquilezles.org/www/articles/frustumcorrect/frustumcorrect.htm">iquilezles.org</a> for an examination of this problem.
+     * 
+     * @param x
+     *          the x-coordinate of the sphere's center
+     * @param y
+     *          the y-coordinate of the sphere's center
+     * @param z
+     *          the z-coordinate of the sphere's center
+     * @param r
+     *          the sphere's radius
+     * @return {@link #INSIDE} if the given sphere is completely inside the frustum, or {@link #INTERSECT} if the sphere intersects
+     *         the frustum, or {@link #OUTSIDE} if the sphere is outside of the frustum
+     */
+    public int intersectSphere(float x, float y, float z, float r) {
+        boolean inside = true;
+        float dist;
+        dist = nxX * x + nxY * y + nxZ * z + nxW;
+        if (dist >= -r) {
+            inside &= dist >= r;
+            dist = pxX * x + pxY * y + pxZ * z + pxW;
+            if (dist >= -r) {
+                inside &= dist >= r;
+                dist = nyX * x + nyY * y + nyZ * z + nyW;
+                if (dist >= -r) {
+                    inside &= dist >= r;
+                    dist = pyX * x + pyY * y + pyZ * z + pyW;
+                    if (dist >= -r) {
+                        inside &= dist >= r;
+                        dist = nzX * x + nzY * y + nzZ * z + nzW;
+                        if (dist >= -r) {
+                            inside &= dist >= r;
+                            dist = pzX * x + pzY * y + pzZ * z + pzW;
+                            if (dist >= -r) {
+                                inside &= dist >= r;
+                                return inside ? INSIDE : INTERSECT;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return OUTSIDE;
+    }
+
+    /**
+     * Test whether the given axis-aligned box is partly or completely within or outside of the frustum defined by <code>this</code> frustum culler.
+     * The box is specified via its <code>min</code> and <code>max</code> corner coordinates.
+     * <p>
+     * The algorithm implemented by this method is conservative. This means that in certain circumstances a <i>false positive</i>
+     * can occur, when the method returns <tt>-1</tt> for boxes that are actually not visible/do not intersect the frustum.
+     * See <a href="http://iquilezles.org/www/articles/frustumcorrect/frustumcorrect.htm">iquilezles.org</a> for an examination of this problem.
+     * 
+     * @param min
+     *          the minimum corner coordinates of the axis-aligned box
+     * @param max
+     *          the maximum corner coordinates of the axis-aligned box
+     * @return <code>true</code> if the axis-aligned box is completely or partly inside of the frustum; <code>false</code> otherwise
+     */
+    public boolean testAab(Vector3f min, Vector3f max) {
+        return testAab(min.x, min.y, min.z, max.x, max.y, max.z);
+    }
+
+    /**
+     * Test whether the given axis-aligned box is partly or completely within or outside of the frustum defined by <code>this</code> frustum culler.
+     * The box is specified via its min and max corner coordinates.
+     * <p>
+     * The algorithm implemented by this method is conservative. This means that in certain circumstances a <i>false positive</i>
+     * can occur, when the method returns <tt>-1</tt> for boxes that are actually not visible/do not intersect the frustum.
+     * See <a href="http://iquilezles.org/www/articles/frustumcorrect/frustumcorrect.htm">iquilezles.org</a> for an examination of this problem.
+     * <p>
+     * Reference: <a href="http://www.cescg.org/CESCG-2002/DSykoraJJelinek/">Efficient View Frustum Culling</a>
+     * 
+     * @param minX
+     *          the x-coordinate of the minimum corner
+     * @param minY
+     *          the y-coordinate of the minimum corner
+     * @param minZ
+     *          the z-coordinate of the minimum corner
+     * @param maxX
+     *          the x-coordinate of the maximum corner
+     * @param maxY
+     *          the y-coordinate of the maximum corner
+     * @param maxZ
+     *          the z-coordinate of the maximum corner
+     * @return <code>true</code> if the axis-aligned box is completely or partly inside of the frustum; <code>false</code> otherwise
+     */
+    public boolean testAab(float minX, float minY, float minZ, float maxX, float maxY, float maxZ) {
+        /*
+         * This is an implementation of the "2.4 Basic intersection test" of the mentioned site.
+         * It does not distinguish between partially inside and fully inside, though, so the test with the 'p' vertex is omitted.
+         */
+        if (nxX * (nxX < 0 ? minX : maxX) + nxY * (nxY < 0 ? minY : maxY) + nxZ * (nxZ < 0 ? minZ : maxZ) >= -nxW) {
+            if (pxX * (pxX < 0 ? minX : maxX) + pxY * (pxY < 0 ? minY : maxY) + pxZ * (pxZ < 0 ? minZ : maxZ) >= -pxW) {
+                if (nyX * (nyX < 0 ? minX : maxX) + nyY * (nyY < 0 ? minY : maxY) + nyZ * (nyZ < 0 ? minZ : maxZ) >= -nyW) {
+                    if (pyX * (pyX < 0 ? minX : maxX) + pyY * (pyY < 0 ? minY : maxY) + pyZ * (pyZ < 0 ? minZ : maxZ) >= -pyW) {
+                        if (nzX * (nzX < 0 ? minX : maxX) + nzY * (nzY < 0 ? minY : maxY) + nzZ * (nzZ < 0 ? minZ : maxZ) >= -nzW) {
+                            if (pzX * (pzX < 0 ? minX : maxX) + pzY * (pzY < 0 ? minY : maxY) + pzZ * (pzZ < 0 ? minZ : maxZ) >= -pzW) {
+                                return true;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Determine whether the given axis-aligned box is partly or completely within or outside of the frustum defined by <code>this</code> frustum culler
      * and, if the box is not inside this frustum, return the index of the plane that culled it.
      * The box is specified via its <code>min</code> and <code>max</code> corner coordinates.
      * <p>
@@ -271,18 +405,15 @@ public class FrustumIntersection {
      * @param max
      *          the maximum corner coordinates of the axis-aligned box
      * @return the index of the first plane that culled the box, if the box does not intersect the frustum;
-     *         or <tt>-1</tt> if the box intersects the frustum.
-     *         The plane index is one of
-     *         {@link #PLANE_NX}, {@link #PLANE_PX},
-     *         {@link #PLANE_NY}, {@link #PLANE_PY},
-     *         {@link #PLANE_NZ} and {@link #PLANE_PZ}
+     *         or {@link #INTERSECT} if the box intersects the frustum, or {@link #INSIDE} if the box is fully inside of the frustum.
+     *         The plane index is one of {@link #PLANE_NX}, {@link #PLANE_PX}, {@link #PLANE_NY}, {@link #PLANE_PY}, {@link #PLANE_NZ} and {@link #PLANE_PZ}
      */
     public int intersectAab(Vector3f min, Vector3f max) {
         return intersectAab(min.x, min.y, min.z, max.x, max.y, max.z);
     }
 
     /**
-     * Determine whether the given axis-aligned box is partly or completely within the viewing frustum defined by <code>this</code> frustum culler
+     * Determine whether the given axis-aligned box is partly or completely within or outside of the frustum defined by <code>this</code> frustum culler
      * and, if the box is not inside this frustum, return the index of the plane that culled it.
      * The box is specified via its min and max corner coordinates.
      * <p>
@@ -305,7 +436,7 @@ public class FrustumIntersection {
      * @param maxZ
      *          the z-coordinate of the maximum corner
      * @return the index of the first plane that culled the box, if the box does not intersect the frustum,
-     *         or {@link #INTERSECT} if the box intersects the frustum, or {@link #INSIDE} if the box is fully inside the frustum.
+     *         or {@link #INTERSECT} if the box intersects the frustum, or {@link #INSIDE} if the box is fully inside of the frustum.
      *         The plane index is one of {@link #PLANE_NX}, {@link #PLANE_PX}, {@link #PLANE_NY}, {@link #PLANE_PY}, {@link #PLANE_NZ} and {@link #PLANE_PZ}
      */
     public int intersectAab(float minX, float minY, float minZ, float maxX, float maxY, float maxZ) {
@@ -344,7 +475,7 @@ public class FrustumIntersection {
     }
 
     /**
-     * Determine whether the given axis-aligned box is partly or completely within the viewing frustum defined by <code>this</code> frustum culler
+     * Determine whether the given axis-aligned box is partly or completely within or outside of the frustum defined by <code>this</code> frustum culler
      * and, if the box is not inside this frustum, return the index of the plane that culled it.
      * The box is specified via its <code>min</code> and <code>max</code> corner coordinates.
      * <p>
@@ -368,7 +499,7 @@ public class FrustumIntersection {
      *          {@link #PLANE_MASK_NY}, {@link #PLANE_MASK_PY}, 
      *          {@link #PLANE_MASK_NZ} and {@link #PLANE_MASK_PZ}
      * @return the index of the first plane that culled the box, if the box does not intersect the frustum,
-     *         or {@link #INTERSECT} if the box intersects the frustum, or {@link #INSIDE} if the box is fully inside the frustum.
+     *         or {@link #INTERSECT} if the box intersects the frustum, or {@link #INSIDE} if the box is fully inside of the frustum.
      *         The plane index is one of {@link #PLANE_NX}, {@link #PLANE_PX}, {@link #PLANE_NY}, {@link #PLANE_PY}, {@link #PLANE_NZ} and {@link #PLANE_PZ}
      */
     public int intersectAabMasked(Vector3f min, Vector3f max, int mask) {
@@ -376,7 +507,7 @@ public class FrustumIntersection {
     }
 
     /**
-     * Determine whether the given axis-aligned box is partly or completely within the viewing frustum defined by <code>this</code> frustum culler
+     * Determine whether the given axis-aligned box is partly or completely within or outside of the frustum defined by <code>this</code> frustum culler
      * and, if the box is not inside this frustum, return the index of the plane that culled it.
      * The box is specified via its min and max corner coordinates.
      * <p>
@@ -410,7 +541,7 @@ public class FrustumIntersection {
      *          {@link #PLANE_MASK_NY}, {@link #PLANE_MASK_PY}, 
      *          {@link #PLANE_MASK_NZ} and {@link #PLANE_MASK_PZ}
      * @return the index of the first plane that culled the box, if the box does not intersect the frustum,
-     *         or {@link #INTERSECT} if the box intersects the frustum, or {@link #INSIDE} if the box is fully inside the frustum.
+     *         or {@link #INTERSECT} if the box intersects the frustum, or {@link #INSIDE} if the box is fully inside of the frustum.
      *         The plane index is one of {@link #PLANE_NX}, {@link #PLANE_PX}, {@link #PLANE_NY}, {@link #PLANE_PY}, {@link #PLANE_NZ} and {@link #PLANE_PZ}
      */
     public int intersectAabMasked(float minX, float minY, float minZ, float maxX, float maxY, float maxZ, int mask) {
