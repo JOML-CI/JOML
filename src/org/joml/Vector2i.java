ea13c5071373cf05148e7c1f978455543d7ac284
httpdigest <kburjack@googlemail.com>
2019-02-23 10:13:59 +0100

    Unify license header and validate during build

--- src/org/joml/Vector2i.java
+++ src/org/joml/Vector2i.java
@@ -1,5 +1,7 @@
 /*
- * (C) Copyright 2015-2019 Richard Greenlees
+ * The MIT License
+ *
+ * Copyright (c) 2015-2019 Richard Greenlees
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -18,7 +20,6 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
- *
  */
 package org.joml;
 
