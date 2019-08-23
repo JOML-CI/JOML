b28ce658990d77d512a0f25dd8297cc14b8ab0a9
AnthoFoxo <realanthowo@gmail.com>
2019-08-22 20:50:13 -0400

    Update Rectangled.java
    Added javadoc to contains and intersects methods

--- src/org/joml/Rectangled.java
+++ src/org/joml/Rectangled.java
@@ -36,64 +36,66 @@ import org.joml.internal.Runtime;
  */
 public class Rectangled {
 
-    public double minX, minY;
-    public double maxX, maxY;
+	public double minX, minY;
+	public double maxX, maxY;
 
-    /**
-     * Create a new {@link Rectangled} with a minimum and maximum corner of <code>(0, 0)</code>.
-     */
-    public Rectangled() {
-    }
+	/**
+	 * Create a new {@link Rectangled} with a minimum and maximum corner of
+	 * <code>(0, 0)</code>.
+	 */
+	public Rectangled() {
+	}
 
-    /**
-     * Create a new {@link Rectangled} as a copy of the given <code>source</code>.
-     * 
-     * @param source
-     *          the {@link Rectangled} to copy from
-     */
-    public Rectangled(Rectangled source) {
-        this.minX = source.minX;
-        this.minY = source.minY;
-        this.maxX = source.maxX;
-        this.maxY = source.maxY;
-    }
+	/**
+	 * Create a new {@link Rectangled} as a copy of the given <code>source</code>.
+	 * 
+	 * @param source the {@link Rectangled} to copy from
+	 */
+	public Rectangled(Rectangled source) {
+		this.minX = source.minX;
+		this.minY = source.minY;
+		this.maxX = source.maxX;
+		this.maxY = source.maxY;
+	}
 
-    /**
-     * Create a new {@link Rectangled} with the given <code>min</code> and <code>max</code> corner coordinates.
-     * 
-     * @param min
-     *          the minimum coordinates
-     * @param max
-     *          the maximum coordinates
-     */
-    public Rectangled(Vector2dc min, Vector2dc max) {
-        this.minX = min.x();
-        this.minY = min.y();
-        this.maxX = max.x();
-        this.maxY = max.y();
-    }
+	/**
+	 * Create a new {@link Rectangled} with the given <code>min</code> and
+	 * <code>max</code> corner coordinates.
+	 * 
+	 * @param min the minimum coordinates
+	 * @param max the maximum coordinates
+	 */
+	public Rectangled(Vector2dc min, Vector2dc max) {
+		this.minX = min.x();
+		this.minY = min.y();
+		this.maxX = max.x();
+		this.maxY = max.y();
+	}
 
-    /**
-     * Create a new {@link Rectangled} with the given minimum and maximum corner coordinates.
-     * 
-     * @param minX
-     *          the x coordinate of the minimum corner
-     * @param minY
-     *          the y coordinate of the minimum corner
-     * @param maxX
-     *          the x coordinate of the maximum corner
-     * @param maxY
-     *          the y coordinate of the maximum corner
-     */
-    public Rectangled(double minX, double minY, double maxX, double maxY) {
-        super();
-        this.minX = minX;
-        this.minY = minY;
-        this.maxX = maxX;
-        this.maxY = maxY;
-    }
+	/**
+	 * Create a new {@link Rectangled} with the given minimum and maximum corner
+	 * coordinates.
+	 * 
+	 * @param minX the x coordinate of the minimum corner
+	 * @param minY the y coordinate of the minimum corner
+	 * @param maxX the x coordinate of the maximum corner
+	 * @param maxY the y coordinate of the maximum corner
+	 */
+	public Rectangled(double minX, double minY, double maxX, double maxY) {
+		super();
+		this.minX = minX;
+		this.minY = minY;
+		this.maxX = maxX;
+		this.maxY = maxY;
+	}
 
-    public boolean intersects(Rectangled r) {
+	/**
+	 * Tests if a this rectangle intersects with another rectangle
+	 * 
+	 * @param r The rectangle to test against
+	 * @return If this rectangle intersects rectangle r
+	 */
+	public boolean intersects(Rectangled r) {
 
 		double tw = maxX - minX;
 		double th = maxY - minY;
@@ -111,12 +113,25 @@ public class Rectangled {
 
 	}
 
+	/**
+	 * Tests if a coordinate is contained within this rectangle
+	 * 
+	 * @param v The vector representing a point
+	 * @return If this rectangle contains the point x, y
+	 */
 	public boolean contains(Vector2dc v) {
 
 		return contains(v.x(), v.y());
 
 	}
 
+	/**
+	 * Tests if a coordinate is contained within this rectangle
+	 * 
+	 * @param x The x coordinate
+	 * @param y The y coordinate
+	 * @return If this rectangle contains the point x, y
+	 */
 	public boolean contains(double x, double y) {
 
 		double w = maxX - minX;
@@ -130,62 +145,56 @@ public class Rectangled {
 		return ((w < minX || w > x) && (h < minY || h > y));
 
 	}
-    
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        long temp;
-        temp = Double.doubleToLongBits(maxX);
-        result = prime * result + (int) (temp ^ (temp >>> 32));
-        temp = Double.doubleToLongBits(maxY);
-        result = prime * result + (int) (temp ^ (temp >>> 32));
-        temp = Double.doubleToLongBits(minX);
-        result = prime * result + (int) (temp ^ (temp >>> 32));
-        temp = Double.doubleToLongBits(minY);
-        result = prime * result + (int) (temp ^ (temp >>> 32));
-        return result;
-    }
 
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        Rectangled other = (Rectangled) obj;
-        if (Double.doubleToLongBits(maxX) != Double.doubleToLongBits(other.maxX))
-            return false;
-        if (Double.doubleToLongBits(maxY) != Double.doubleToLongBits(other.maxY))
-            return false;
-        if (Double.doubleToLongBits(minX) != Double.doubleToLongBits(other.minX))
-            return false;
-        if (Double.doubleToLongBits(minY) != Double.doubleToLongBits(other.minY))
-            return false;
-        return true;
-    }
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		long temp;
+		temp = Double.doubleToLongBits(maxX);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		temp = Double.doubleToLongBits(maxY);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		temp = Double.doubleToLongBits(minX);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		temp = Double.doubleToLongBits(minY);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
 
-    /**
-     * Return a string representation of this rectangle.
-     * <p>
-     * This method creates a new {@link DecimalFormat} on every invocation with the format string "<code>0.000E0;-</code>".
-     * 
-     * @return the string representation
-     */
-    public String toString() {
-        return Runtime.formatNumbers(toString(Options.NUMBER_FORMAT));
-    }
+	public boolean equals(Object obj) {
+		if (this == obj) return true;
+		if (obj == null) return false;
+		if (getClass() != obj.getClass()) return false;
+		Rectangled other = (Rectangled) obj;
+		if (Double.doubleToLongBits(maxX) != Double.doubleToLongBits(other.maxX)) return false;
+		if (Double.doubleToLongBits(maxY) != Double.doubleToLongBits(other.maxY)) return false;
+		if (Double.doubleToLongBits(minX) != Double.doubleToLongBits(other.minX)) return false;
+		if (Double.doubleToLongBits(minY) != Double.doubleToLongBits(other.minY)) return false;
+		return true;
+	}
 
-    /**
-     * Return a string representation of this rectangle by formatting the vector components with the given {@link NumberFormat}.
-     * 
-     * @param formatter
-     *          the {@link NumberFormat} used to format the vector components with
-     * @return the string representation
-     */
-    public String toString(NumberFormat formatter) {
-        return "(" + formatter.format(minX) + " " + formatter.format(minY) + ") < "
-             + "(" + formatter.format(maxX) + " " + formatter.format(maxY) + ")";
-    }
+	/**
+	 * Return a string representation of this rectangle.
+	 * <p>
+	 * This method creates a new {@link DecimalFormat} on every invocation with the
+	 * format string "<code>0.000E0;-</code>".
+	 * 
+	 * @return the string representation
+	 */
+	public String toString() {
+		return Runtime.formatNumbers(toString(Options.NUMBER_FORMAT));
+	}
+
+	/**
+	 * Return a string representation of this rectangle by formatting the vector
+	 * components with the given {@link NumberFormat}.
+	 * 
+	 * @param formatter the {@link NumberFormat} used to format the vector
+	 *                  components with
+	 * @return the string representation
+	 */
+	public String toString(NumberFormat formatter) {
+		return "(" + formatter.format(minX) + " " + formatter.format(minY) + ") < " + "(" + formatter.format(maxX) + " " + formatter.format(maxY) + ")";
+	}
 
 }
