b5a30595989a0e442f8e3f406d3930dd49421a8c
Michael <polli104@mail.chapman.edu>
2020-04-27 17:10:42 -0700

    added test cases for rectangle

---
+++ test/org/joml/test/RectangleiTest.java
@@ -0,0 +1,81 @@
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2020 JOML.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.joml.test;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+import org.joml.Rectanglei;
+import org.joml.Vector2f;
+import org.joml.Vector2i;
+
+/**
+ * Tests for the {@link Rectanglei} class.
+ *
+ * @author Michael Pollind
+ */
+public class RectangleiTest extends TestCase {
+
+    public void testRectangleContainsPoints() {
+        Rectanglei rect = new Rectanglei(0, 0, 3, 3);
+
+        Assert.assertTrue(rect.isValid());
+        Assert.assertTrue(rect.contains(new Vector2i(0, 0)));
+        Assert.assertFalse(rect.contains(new Vector2i(-1, -1)));
+        Assert.assertFalse(rect.contains(new Vector2i(4, 4)));
+    }
+
+    public void testRectangleIntersection() {
+        Rectanglei first = new Rectanglei(0, 0, 3, 3);
+        Rectanglei second = new Rectanglei(-1, -1, 2, 2);
+
+        // is valid
+        Assert.assertTrue(first.isValid());
+        Assert.assertTrue(second.isValid());
+
+        Assert.assertFalse(first.contains(second));
+        Assert.assertFalse(second.contains(first));
+
+        Assert.assertTrue(first.intersects(second));
+        Assert.assertTrue(second.intersects(first));
+        Assert.assertEquals(first.intersection(second, new Rectanglei()), new Rectanglei(0, 0, 2, 2));
+
+    }
+
+    public void testRectangleContains() {
+        Rectanglei first = new Rectanglei(-1, -1, 2, 2);
+        Rectanglei second = new Rectanglei(0, 0, 1, 1);
+        Assert.assertTrue(first.contains(second));
+        Assert.assertFalse(second.contains(first));
+
+        Assert.assertTrue(first.intersects(second));
+        Assert.assertTrue(second.intersects(first));
+
+        Assert.assertEquals(first.intersection(second, new Rectanglei()), new Rectanglei(0, 0, 1, 1));
+    }
+
+    public void testZeroSizeRectangle() {
+        Rectanglei rect = new Rectanglei(0, 0, 0, 0);
+        Assert.assertFalse(rect.isValid());
+    }
+}
