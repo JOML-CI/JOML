340345a03cb255c001a236389d256ae3f2e901cd
Michael <polli104@mail.chapman.edu>
2020-08-18 08:26:38 -0700

    update copyright and comment to header

--- src/org/joml/AABBfc.java
+++ src/org/joml/AABBfc.java
@@ -1,7 +1,7 @@
 /*
  * The MIT License
  *
- * Copyright (c) 2017-2020 JOML
+ * Copyright (c) 2020 JOML
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -25,6 +25,9 @@ package org.joml;
 
 import java.text.NumberFormat;
 
+/**
+ * Interface to a read-only view of a AABB of floats
+ */
 public interface AABBfc {
 
     /**
--- src/org/joml/AABBic.java
+++ src/org/joml/AABBic.java
@@ -1,7 +1,7 @@
 /*
  * The MIT License
  *
- * Copyright (c) 2017-2020 JOML
+ * Copyright (c) 2020 JOML
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -23,6 +23,9 @@
  */
 package org.joml;
 
+/**
+ * Interface to a read-only view of a AABB of ints
+ */
 public interface AABBic {
 
     /**
