6a3bf9781a4f82cfc8cb2dde0ee22ecce30fb93e
Michael Pollind <mpollind@gmail.com>
2020-09-26 23:07:14 -0700

    correct test

--- test/org/joml/test/Vector3iTest.java
+++ test/org/joml/test/Vector3iTest.java
@@ -38,7 +38,7 @@ public class Vector3iTest extends TestCase {
     public static void testVector3iRounding() {
         Vector3i v1 = new Vector3i(0.0f,.6f,.7f, RoundingMode.FLOOR);
         Vector3i v2 = new Vector3i(9.5f,1.6f,5.0f, RoundingMode.FLOOR);
-        Vector3i v3 = new Vector3i(new Vector3f(9.5f,1.6f,5.0f), RoundingMode.FLOOR);
+        Vector3i v3 = new Vector3i(new Vector3f(0.0f,.6f,.7f), RoundingMode.FLOOR);
         Vector3i v4 = new Vector3i(new Vector3d(9.5f,1.6f,5.0f), RoundingMode.FLOOR);
 
         assertEquals(v1, new Vector3i(0,0,0));
