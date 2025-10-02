d9fdf07d530c623422d0c3b2756a186419ba7001
木葉 Scarlet <93977077+mukjepscarlet@users.noreply.github.com>
2025-10-02 13:18:30 +0800

    refactor Kotlin extensions
    1. Make them hide to Java
    2. Make them inline for better performance

--- src/main/kotlin/org/joml/Matrix3x2d.kt
+++ src/main/kotlin/org/joml/Matrix3x2d.kt
@@ -1,18 +1,20 @@
+@file:Suppress("NOTHING_TO_INLINE")
+@file:JvmName("-Matrix3x2d")
 package org.joml
 
 import java.nio.ByteBuffer
 import java.nio.DoubleBuffer
 
 /* Operators */
-public operator fun Matrix3x2dc.times(m: Matrix3x2dc): Matrix3x2d = mul(m, Matrix3x2d())
-public operator fun Matrix3x2dc.times(v: Vector3dc): Vector3d = transform(v, Vector3d())
-public operator fun Matrix3x2d.timesAssign(m: Matrix3x2dc) { mul(m) }
+public inline operator fun Matrix3x2dc.times(m: Matrix3x2dc): Matrix3x2d = mul(m, Matrix3x2d())
+public inline operator fun Matrix3x2dc.times(v: Vector3dc): Vector3d = transform(v, Vector3d())
+public inline operator fun Matrix3x2d.timesAssign(m: Matrix3x2dc) { mul(m) }
 
 /* Buffer Operations */
-public fun ByteBuffer.getMatrix3x2d(v: Matrix3x2d): Matrix3x2d = v.set(this)
-public fun ByteBuffer.putMatrix3x2d(v: Matrix3x2d): ByteBuffer = v.get(this)
-public fun ByteBuffer.putMatrix3x2d(index: Int, v: Matrix3x2d): ByteBuffer = v.get(index, this)
-public fun DoubleBuffer.getMatrix3x2d(): Matrix3x2d = Matrix3x2d(this)
-public fun DoubleBuffer.getMatrix3x2d(v: Matrix3x2d): Matrix3x2d = v.set(this)
-public fun DoubleBuffer.putMatrix3x2d(v: Matrix3x2d): DoubleBuffer = v.get(this)
-public fun DoubleBuffer.putMatrix3x2d(index: Int, v: Matrix3x2d): DoubleBuffer = v.get(index, this)
+public inline fun ByteBuffer.getMatrix3x2d(v: Matrix3x2d): Matrix3x2d = v.set(this)
+public inline fun ByteBuffer.putMatrix3x2d(v: Matrix3x2d): ByteBuffer = v.get(this)
+public inline fun ByteBuffer.putMatrix3x2d(index: Int, v: Matrix3x2d): ByteBuffer = v.get(index, this)
+public inline fun DoubleBuffer.getMatrix3x2d(): Matrix3x2d = Matrix3x2d(this)
+public inline fun DoubleBuffer.getMatrix3x2d(v: Matrix3x2d): Matrix3x2d = v.set(this)
+public inline fun DoubleBuffer.putMatrix3x2d(v: Matrix3x2d): DoubleBuffer = v.get(this)
+public inline fun DoubleBuffer.putMatrix3x2d(index: Int, v: Matrix3x2d): DoubleBuffer = v.get(index, this)
--- src/main/kotlin/org/joml/Vector4f.kt
+++ src/main/kotlin/org/joml/Vector4f.kt
@@ -1,46 +1,48 @@
+@file:Suppress("NOTHING_TO_INLINE")
+@file:JvmName("-Vector4f")
 package org.joml
 
 import java.nio.ByteBuffer
 import java.nio.FloatBuffer
 
 /* Operators */
-public operator fun Vector4fc.component1(): Float = x()
-public operator fun Vector4fc.component2(): Float = y()
-public operator fun Vector4fc.component3(): Float = z()
-public operator fun Vector4fc.component4(): Float = w()
-public operator fun Vector4fc.plus(v: Vector4fc): Vector4f = add(v, Vector4f())
-public operator fun Vector4f.plusAssign(v: Vector4fc) { add(v) }
-public operator fun Vector4fc.minus(v: Vector4fc): Vector4f = sub(v, Vector4f())
-public operator fun Vector4f.minusAssign(v: Vector4fc) { sub(v) }
-public operator fun Vector4fc.times(v: Vector4fc): Vector4f = mul(v, Vector4f())
-public operator fun Vector4fc.times(s: Float): Vector4f = mul(s, Vector4f())
-public operator fun Vector4fc.times(m: Matrix4fc): Vector4f = mul(m, Vector4f())
-public operator fun Vector4fc.times(m: Matrix4x3fc): Vector4f = mul(m, Vector4f())
-public operator fun Vector4f.timesAssign(v: Vector4fc) { mul(v) }
-public operator fun Vector4f.timesAssign(s: Float) { mul(s) }
-public operator fun Vector4f.timesAssign(m: Matrix4fc) { mul(m) }
-public operator fun Vector4f.timesAssign(m: Matrix4x3fc) { mul(m) }
-public operator fun Vector4fc.div(v: Vector4fc): Vector4f = div(v, Vector4f())
-public operator fun Vector4fc.div(s: Float): Vector4f = div(s, Vector4f())
-public operator fun Vector4f.divAssign(v: Vector4fc) { div(v) }
-public operator fun Vector4f.divAssign(s: Float) { div(s) }
-public operator fun Vector4fc.unaryMinus(): Vector4f = negate(Vector4f())
-public infix fun Vector4fc.dot(v: Vector4fc): Float = dot(v)
-public infix fun Vector4fc.distance(v: Vector4fc): Float = distance(v)
-public infix fun Vector4fc.distanceSquared(v: Vector4fc): Float = distanceSquared(v)
-public infix fun Vector4fc.angleCos(v: Vector4fc): Float = angleCos(v)
-public infix fun Vector4fc.angle(v: Vector4fc): Float = angle(v)
+public inline operator fun Vector4fc.component1(): Float = x()
+public inline operator fun Vector4fc.component2(): Float = y()
+public inline operator fun Vector4fc.component3(): Float = z()
+public inline operator fun Vector4fc.component4(): Float = w()
+public inline operator fun Vector4fc.plus(v: Vector4fc): Vector4f = add(v, Vector4f())
+public inline operator fun Vector4f.plusAssign(v: Vector4fc) { add(v) }
+public inline operator fun Vector4fc.minus(v: Vector4fc): Vector4f = sub(v, Vector4f())
+public inline operator fun Vector4f.minusAssign(v: Vector4fc) { sub(v) }
+public inline operator fun Vector4fc.times(v: Vector4fc): Vector4f = mul(v, Vector4f())
+public inline operator fun Vector4fc.times(s: Float): Vector4f = mul(s, Vector4f())
+public inline operator fun Vector4fc.times(m: Matrix4fc): Vector4f = mul(m, Vector4f())
+public inline operator fun Vector4fc.times(m: Matrix4x3fc): Vector4f = mul(m, Vector4f())
+public inline operator fun Vector4f.timesAssign(v: Vector4fc) { mul(v) }
+public inline operator fun Vector4f.timesAssign(s: Float) { mul(s) }
+public inline operator fun Vector4f.timesAssign(m: Matrix4fc) { mul(m) }
+public inline operator fun Vector4f.timesAssign(m: Matrix4x3fc) { mul(m) }
+public inline operator fun Vector4fc.div(v: Vector4fc): Vector4f = div(v, Vector4f())
+public inline operator fun Vector4fc.div(s: Float): Vector4f = div(s, Vector4f())
+public inline operator fun Vector4f.divAssign(v: Vector4fc) { div(v) }
+public inline operator fun Vector4f.divAssign(s: Float) { div(s) }
+public inline operator fun Vector4fc.unaryMinus(): Vector4f = negate(Vector4f())
+public inline infix fun Vector4fc.dot(v: Vector4fc): Float = dot(v)
+public inline infix fun Vector4fc.distance(v: Vector4fc): Float = distance(v)
+public inline infix fun Vector4fc.distanceSquared(v: Vector4fc): Float = distanceSquared(v)
+public inline infix fun Vector4fc.angleCos(v: Vector4fc): Float = angleCos(v)
+public inline infix fun Vector4fc.angle(v: Vector4fc): Float = angle(v)
 
 /* Buffer Operations */
-public fun ByteBuffer.getVector4f(): Vector4f = Vector4f(this)
-public fun ByteBuffer.getVector4f(index: Int): Vector4f = Vector4f(index, this)
-public fun ByteBuffer.getVector4f(v: Vector4f): Vector4f = v.set(this)
-public fun ByteBuffer.getVector4f(index: Int, v: Vector4f): Vector4f = v.set(index, this)
-public fun ByteBuffer.putVector4f(v: Vector4f): ByteBuffer = v.get(this)
-public fun ByteBuffer.putVector4f(index: Int, v: Vector4f): ByteBuffer = v.get(index, this)
-public fun FloatBuffer.getVector4f(): Vector4f = Vector4f(this)
-public fun FloatBuffer.getVector4f(index: Int): Vector4f = Vector4f(index, this)
-public fun FloatBuffer.getVector4f(v: Vector4f): Vector4f = v.set(this)
-public fun FloatBuffer.getVector4f(index: Int, v: Vector4f): Vector4f = v.set(index, this)
-public fun FloatBuffer.putVector4f(v: Vector4f): FloatBuffer = v.get(this)
-public fun FloatBuffer.putVector4f(index: Int, v: Vector4f): FloatBuffer = v.get(index, this)
+public inline fun ByteBuffer.getVector4f(): Vector4f = Vector4f(this)
+public inline fun ByteBuffer.getVector4f(index: Int): Vector4f = Vector4f(index, this)
+public inline fun ByteBuffer.getVector4f(v: Vector4f): Vector4f = v.set(this)
+public inline fun ByteBuffer.getVector4f(index: Int, v: Vector4f): Vector4f = v.set(index, this)
+public inline fun ByteBuffer.putVector4f(v: Vector4f): ByteBuffer = v.get(this)
+public inline fun ByteBuffer.putVector4f(index: Int, v: Vector4f): ByteBuffer = v.get(index, this)
+public inline fun FloatBuffer.getVector4f(): Vector4f = Vector4f(this)
+public inline fun FloatBuffer.getVector4f(index: Int): Vector4f = Vector4f(index, this)
+public inline fun FloatBuffer.getVector4f(v: Vector4f): Vector4f = v.set(this)
+public inline fun FloatBuffer.getVector4f(index: Int, v: Vector4f): Vector4f = v.set(index, this)
+public inline fun FloatBuffer.putVector4f(v: Vector4f): FloatBuffer = v.get(this)
+public inline fun FloatBuffer.putVector4f(index: Int, v: Vector4f): FloatBuffer = v.get(index, this)
